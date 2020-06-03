563acf3eb277feaa760cfc99068888689691e94c
Søren Hartvig Jensen <48486246+SorenHartvigJensen@users.noreply.github.com>
2020-06-03 10:00:08 +0200

    Added using System.Globalization;

--- DocFx.Plugin.LastModified/LastModifiedPostProcessor.cs
+++ DocFx.Plugin.LastModified/LastModifiedPostProcessor.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Globalization;
 using DocFx.Plugin.LastModified.Helpers;
 using HtmlAgilityPack;
 using LibGit2Sharp;
